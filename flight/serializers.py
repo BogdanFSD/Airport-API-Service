9d62fa1019ade78b2b73887b9fe4600b4d1b7e0e
BohdanFSD-temp <luitko009@gmail.com>
2023-08-21 19:09:08 +0000

    Created serializers for flight app

---
+++ flight/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from flight.models import AirplaneType, Airplane, Crew, Flight
+
+
+class AirplaneTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AirplaneType
+        fields = ("id", "name")
+
+
+class AirplaneSerializer(serializers.ModelSerializer):
+    airplane_type = serializers.SlugRelatedField(
+        many=False,
+        read_only=False,
+        slug_field="name",
+        queryset=AirplaneType.objects.all(),
+    )
+
+    class Meta:
+        model = Airplane
+        fields = ("id", "name", "rows", "seats_in_row", "airplane_type")
+
+
+class CrewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Crew
+        fields = ("id", "first_name", "last_name")
+
+
+class FlightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Flight
+        fields = (
+            "id",
+            "crew",
+            "route",
+            "airplane",
+            "departure_time",
+            "arrival_time",
+        )
