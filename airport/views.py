0ba4ec13dec4b9180fc4c3cc8588f571805fc4e3
BohdanFSD-temp <luitko009@gmail.com>
2023-08-22 05:53:09 +0000

    Added permissions

--- airport/views.py
+++ airport/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-
+from airport.permissions import IsAdminOrIfUserReadOnly
 from airport.models import Airport, Route
 from airport.serializers import AirportSerializer, RouteSerializer
 
@@ -8,8 +9,12 @@ from airport.serializers import AirportSerializer, RouteSerializer
 class AirportViewSet(viewsets.ModelViewSet):
     queryset = Airport.objects.all()
     serializer_class = AirportSerializer
+    permission_classes = (IsAdminOrIfUserReadOnly,)
+    authentication_classes = (JWTAuthentication,)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
     queryset = Route.objects.all().select_related("source", "destination")
     serializer_class = RouteSerializer
+    permission_classes = (IsAdminOrIfUserReadOnly,)
+    authentication_classes = (JWTAuthentication,)
