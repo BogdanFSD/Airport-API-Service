227a0318626467f00d22e2d7e78d6eb21549ac96
BohdanFSD-temp <luitko009@gmail.com>
2023-08-22 05:23:16 +0000

    Changed user admin

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -13,6 +13,7 @@ import os
 
 from pathlib import Path
 from dotenv import load_dotenv
+from datetime import timedelta
 
 
 load_dotenv()
@@ -144,3 +145,15 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+}
---
+++ user/urls.py
@@ -0,0 +1,11 @@
+from rest_framework import routers
+
+
+app_name = "user"
+
+
+router = routers.DefaultRouter()
+# router.register()
+
+
+urlpatterns = [] + router.urls
