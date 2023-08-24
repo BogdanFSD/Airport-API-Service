9a701caacf0a758d383c42832bd4ed1fae49f929
BohdanFSD-temp <luitko009@gmail.com>
2023-08-24 06:08:41 +0000

    Added documentation, updated views

--- airport_api/settings.py
+++ airport_api/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework_simplejwt",
+    "drf_spectacular",
     "debug_toolbar",
     "rest_framework",
     "flight",
@@ -150,9 +152,10 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
--- flight/views.py
+++ flight/views.py
@@ -1,34 +1,71 @@
-from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema_view, extend_schema
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from airport.permissions import IsAdminOrIfUserReadOnly
 from flight.models import AirplaneType, Airplane, Crew, Flight
-from flight.serializers import AirplaneTypeSerializer, AirplaneSerializer, CrewSerializer, FlightSerializer
+from flight.serializers import (
+    AirplaneTypeSerializer,
+    AirplaneSerializer,
+    CrewSerializer,
+    FlightSerializer,
+)
 
 
-class AirplaneTypeViewSet(viewsets.ModelViewSet):
+@extend_schema_view(
+    list=extend_schema(description="All airplane type endpoint in the db"),
+    create=extend_schema(description="Creating airplane type endpoint"),
+)
+class AirplaneTypeViewSet(
+    mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet
+):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
     permission_classes = (IsAdminOrIfUserReadOnly,)
     authentication_classes = (JWTAuthentication,)
 
 
-class AirplaneViewSet(viewsets.ModelViewSet):
-    queryset = Airplane.objects.all().select_related("airplane_type")
+@extend_schema_view(
+    list=extend_schema(description="All airplane endpoint in the db"),
+    retrieve=extend_schema(description="Specific airplane endpoint"),
+    create=extend_schema(description="Creating airplane endpoint"),
+)
+class AirplaneViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Airplane.objects.select_related("airplane_type")
     serializer_class = AirplaneSerializer
     permission_classes = (IsAdminOrIfUserReadOnly,)
     authentication_classes = (JWTAuthentication,)
 
 
-class CrewViewSet(viewsets.ModelViewSet):
+@extend_schema_view(
+    list=extend_schema(description="All crew endpoint in the db"),
+    create=extend_schema(description="Creating crew endpoint"),
+)
+class CrewViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = (IsAdminOrIfUserReadOnly,)
     authentication_classes = (JWTAuthentication,)
 
 
-class FlightViewSet(viewsets.ModelViewSet):
-    queryset = Flight.objects.all().prefetch_related("crew").select_related("route")
+@extend_schema_view(
+    list=extend_schema(description="All flight endpoint in the db"),
+    retrieve=extend_schema(description="Specific flight endpoint"),
+    create=extend_schema(description="Creating flight endpoint"),
+)
+class FlightViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Flight.objects.prefetch_related("crew").select_related("route")
     serializer_class = FlightSerializer
     permission_classes = (IsAdminOrIfUserReadOnly,)
     authentication_classes = (JWTAuthentication,)
